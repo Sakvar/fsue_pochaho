8683d9ef61161f50f64dd3ffe88a6cabd4b530d1
Sakvar <sakvar@gmail.com>
2026-07-09 22:30:45 -0400

    feat: add phaser config

---
+++ src/game/phaserConfig.ts
@@ -0,0 +1,18 @@
+import Phaser from 'phaser';
+import { GameScene } from './scenes/GameScene';
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  backgroundColor: '#10151c',
+  parent: 'game-root',
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 1280,
+    height: 720,
+  },
+  scene: [GameScene],
+  pixelArt: true,
+};
+
+export default config;
