e8198c7b7e4c6ac9fae9f104e50e1da3564da618
Sakvar <sakvar@gmail.com>
2026-07-09 22:28:24 -0400

    chore: add Vite config

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+
+export default defineConfig({
+  base: repoName ? `/${repoName}/` : '/',
+  server: {
+    port: 5173,
+  },
+});
